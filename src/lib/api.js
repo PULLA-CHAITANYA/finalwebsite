a48cee29a73acaecb11ca63a70acb9cbd2f1ece2
PULLA-CHAITANYA 111622201083 <119579091+PULLA-CHAITANYA@users.noreply.github.com>
2025-09-01 19:54:45 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "finalbackend-hherc0arefhubdbk.centralindia-01.azurewebsites.net",
 });
 
 api.interceptors.request.use((cfg) => {
