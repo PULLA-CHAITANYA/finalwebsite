78ddedcf35e131e6f48f4c02a70f5f914cbbb0d5
PULLA-CHAITANYA 111622201083 <119579091+PULLA-CHAITANYA@users.noreply.github.com>
2025-09-01 19:55:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "finalbackend-hherc0arefhubdbk.centralindia-01.azurewebsites.net",
         changeOrigin: true,
       },
     },
